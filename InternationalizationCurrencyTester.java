332142f70d2f02990ad4b72c68a8ea84f8e93308
raavi139 <raavi139@users.noreply.github.com>
2017-04-03 15:05:31 -0700

    Create InternationalizationCurrencyTester.java

---
+++ InternationalizationCurrencyTester.java
@@ -0,0 +1,20 @@
+package com.nact.broker;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class InternationalizationCurrencyTester {
+
+    /**
+     * example demonstrates Indian currency symbol and custom format.
+     */
+    public static void main(String[] args) throws Exception {
+        double amount = 200100040.12;
+        NumberFormat nfci = NumberFormat.getCurrencyInstance(new Locale("hi", "IN"));
+        DecimalFormat df_nfci = (DecimalFormat)nfci;
+        df_nfci.applyPattern("\u20B9 ###,###.##");
+        String indianMoney = df_nfci.format(amount);
+        System.out.println(indianMoney);
+    }
+}
